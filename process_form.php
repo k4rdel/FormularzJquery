1c90d00746b8468be72b7b7842caf0456727f9f6
oskar <67971701+k4rdel@users.noreply.github.com>
2023-12-11 22:40:01 +0100

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,10 +1,19 @@
 <?php
-    $host = 'localhost';
-    $db = 'db1';
-    $user = 'root';
-    $password = '';
+$host = 'localhost';
+$db = 'db1';
+$user = 'root';
+$password = '';
 
-    $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Błąd połączenia z bazą danych: ' . $e->getMessage());
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $dob = $_POST['dob'];
@@ -19,4 +28,5 @@
     $stmt->execute([$firstName, $lastName, $dob, $email, $phone, $voivodeship, $gender, $newsletter]);
 
     echo 'Dane zostały pomyślnie zapisane w bazie danych.';
+}
 ?>
